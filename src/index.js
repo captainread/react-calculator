c0e5c01a53b64a895b715cabe5ed43af35fb7b40
Alice Read <aread773@gmail.com>
2023-01-16 16:30:36 +0000

    add favicon

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import React, { StrictMode } from 'react';
 
 import App from './App';
 import { ColorModeScript } from '@chakra-ui/react';
+import Favicon from 'react-favicon';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
@@ -11,6 +12,7 @@ const root = ReactDOM.createRoot(container);
 root.render(
   <StrictMode>
     <ColorModeScript />
+    <Favicon url="public/favicon.svg" />
     <App />
   </StrictMode>
 );
